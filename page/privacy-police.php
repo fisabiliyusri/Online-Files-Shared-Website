b9fe42eeba8c8391789a5e1bf6aa05474fe6574b
Muhammad Mahyuni <103715453+iniremon@users.noreply.github.com>
2022-06-08 12:19:30 +0700

    Add files via upload
    folder untuk menempatkan file info tentang website seperti about, contact dan privacy police.

---
+++ page/privacy-police.php
@@ -0,0 +1,122 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <title>Files Upload and Download</title>
+  </head>
+  <body>
+
+  
+
+
+<!-- navbar -->
+<nav class="navbar navbar-dark bg-dark shadow-sm">
+  <div class="container">
+    <a class="navbar-brand font-monospace fw-bold" href="#">YOUNIFILES</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav ms-auto">
+        <a class="nav-link font-monospace" aria-current="page" href="index.php"><i class="bi bi-cloud-arrow-up-fill"></i> UPLOAD</a>
+        <a class="nav-link font-monospace" href="downloads.php"><i class="bi bi-cloud-arrow-down-fill"></i> DOWNLOAD</a>
+        <a class="nav-link font-monospace" href="search.php"><i class="bi bi-search"></i> SEARCH</a>
+      </div>
+    </div>
+  </div>
+</nav>
+<!-- akhir navbar -->
+
+
+
+
+
+
+
+<div class="container mt-3">
+<table class="table table-bordered border-dark">
+  <thead class="table-dark">
+  <th class="font-monospace">PRIVACY POLICE</th>
+  </thead>
+  <tbody>
+  <tr><td><h5>Privacy Policy for YOUNIFILES</h5>
+
+<p>At Beardice, accessible from www.beardice.eu.org, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Beardice and how we use it.</p>
+
+<p>If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.</p>
+
+<h5>Log Files</h5>
+
+<p>Beardice follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.</p>
+
+<h5>Cookies and Web Beacons</h5>
+
+<p>Like any other website, Beardice uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.</p>
+
+<p>For more general information on cookies, please read <a href="https://www.generateprivacypolicy.com/#cookies">"Cookies"</a>.</p>
+
+<h5>Google DoubleClick DART Cookie</h5>
+
+<p>Google is one of a third-party vendor on our site. It also uses cookies, known as DART cookies, to serve ads to our site visitors based upon their visit to www.website.com and other sites on the internet. However, visitors may choose to decline the use of DART cookies by visiting the Google ad and content network Privacy Policy at the following URL – <a href="https://policies.google.com/technologies/ads">https://policies.google.com/technologies/ads</a></p>
+
+
+<h5>Privacy Policies</h5>
+
+<p>You may consult this list to find the Privacy Policy for each of the advertising partners of Beardice.</p>
+
+<p>Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Beardice, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
+
+<p>Note that Beardice has no access to or control over these cookies that are used by third-party advertisers.</p>
+
+<h5>Third Party Privacy Policies</h5>
+
+<p>Beardice's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. </p>
+
+<p>You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites. What Are Cookies?</p>
+
+<h5>Children's Information</h5>
+
+<p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
+
+<p>Beardice does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
+
+<h5>Online Privacy Policy Only</h5>
+
+<p>This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Beardice. This policy is not applicable to any information collected offline or via channels other than this website.</p>
+
+<h5>Consent</h5>
+
+<p>By using our website, you hereby consent to our Privacy Policy and agree to its Terms and Conditions.</p></td></tr>
+
+</tbody>
+</table>
+  
+
+<!-- footer -->
+<table class="table table-bordered border-dark">
+<tbody>
+  <td><div class="container text-center"><a href="#" style="text-decoration:none;" class="link-dark">Privacy Police</a> | 
+  <a href="#" style="text-decoration:none;" class="link-dark">Disclamer</a> | 
+  <a href="#" style="text-decoration:none;" class="link-dark">Contact</a>
+</div>
+</td>
+</tbody>
+<tbody>
+<td><div class="container text-center">
+© 2022, youfiles.com
+<br>
+online file sharing website
+</div>
+</td>
+</tbody>
+</table>
+<!-- akhir footer -->
+
+   
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+  </body>
+</html>
